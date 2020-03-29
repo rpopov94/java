c2cdd63eab0bd1b50d390e66cb29cbe6d25dd642
rpopov94 <52166414+rpopov94@users.noreply.github.com>
2020-03-29 16:53:51 +0300

    add three level in game

--- Guess_the_number.java
+++ Guess_the_number.java
@@ -1,20 +1,27 @@
 package com.company;
 
+package com.company;
+
+import java.util.Scanner;
+
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Ваша задача угадать число.");
-        int range = 30;
-        int number = (int) (Math.random() * range);
-        playLevel(range, number);
+        for (int i = 10; i <=30; i+=10){
+            System.out.println("LEVEL " + i/10);
+            playLevel(i);
+        }
+        System.out.println("You win!");
         scanner.close();
     }
 
-    private static void playLevel(int range, int number){
+    private static void playLevel(int i){
+        int number = (int) (Math.random() * i);
         while (true) {
-            System.out.println("Угадайте число от 0 до " + range);
+            System.out.println("Угадайте число от 0 до " + i);
             int input_number = scanner.nextInt();
             if (input_number == number) {
                 System.out.println("Вы угадали.");
@@ -27,4 +34,3 @@ public class Main {
         }
     }
 }
-
